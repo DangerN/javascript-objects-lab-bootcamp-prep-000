7afbfb8ad5f96336210bbed4a4e70b6728207bc7
convivial-system-4430 <cdneal091@gmail.com>
2019-01-25 06:34:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value)
 { 
-  const newObj = Object.assign({}, object,)
+  const newObj = Object.assign({}, object, {[key]:value})
   return(newObj)
 }
