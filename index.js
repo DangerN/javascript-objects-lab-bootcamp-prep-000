f008dcf9585123cac0c8ee3cf6e1e5c393fb438d
convivial-system-4430 <cdneal091@gmail.com>
2019-01-25 06:36:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value)
 { 
-  const newObj = Object.assign({}, object, {[key]:value})
-  return(newObj)
+  const newObj = Object.assign({}, object, {[key]:value});
+  return(newObj);
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+{
+  object[key] = value;
+  return(object);
 }
